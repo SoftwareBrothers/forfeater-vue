89f563ff15091c231f0a4a646a34d1921831ea1f
Marek Firlejczyk <marek.firlejczyk@rst-it.com>
2018-07-31 09:28:10 +0200

    add vee-valdiate

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import VeeValidate from "vee-validate";
 import router from "@/router";
 
 import App from "./App.vue";
@@ -9,6 +10,8 @@ import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 library.add(faEdit)
 library.add(faTrash)
+
+Vue.use(VeeValidate);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false;
