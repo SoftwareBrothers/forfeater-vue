c4c3b896a0bb8e6e71c0699445a6fea421ca7d67
Paweł Lorenc <pawel.lorenc@rst-it.com>
2018-08-02 15:46:58 +0200

    gary move out without comma szwagier

--- src/components/PageNotFound.vue
+++ src/components/PageNotFound.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="row pt-3">
       <div class="col-sm">
-        <h1 class="text-center">Gary, move out!</h1>
+        <h1 class="text-center">Gary move out!</h1>
         <div class="text-center">
           <iframe width="560" height="315" src="https://www.youtube.com/embed/WcYe1Sd8H_U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
         </div>
