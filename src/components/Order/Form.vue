1e52f6c8a8fca11f7bd531b2452e13547757d943
Paweł Lorenc <pawel.lorenc@rst-it.com>
2018-08-02 16:29:21 +0200

    plan for managing products when order is creating

--- src/components/Order/Form.vue
+++ src/components/Order/Form.vue
@@ -3,9 +3,9 @@
     <div class="form-row">
       <div class="form-group col-md-12 custom-control">
         <label for="name">Vendor</label>
-        <select v-model="Order.vendorId" v-validate="'required'" name="vendorId" class="custom-select">
-              <option v-for="(vendor,key) in vendors" :key="key" :value="vendor.id">{{ vendor.name }}</option>
-            </select>
+        <select v-model="Order.vendorId" v-validate="'required'" name="vendorId" @change="myFunction()" class="custom-select">
+                <option v-for="(vendor,key) in vendors" :key="key" :value="vendor.id">{{ vendor.name }}</option>
+              </select>
         <div class="invalid-feedback-not-work">{{ errors.first('vendorId')}}</div>
       </div>
     </div>
@@ -13,29 +13,14 @@
       <div class="form-group col-md-6 custom-control">
         <label for="name">Deadline</label>
         <div>
-          <flat-pickr 
-                v-model="Order.deadlineAt"
-                v-validate="'date_format:YYYY-MM-DD HH:mm|required'"
-                :config="config"                                                          
-                class="form-control" 
-                placeholder="Select date"
-                name="deadlineAt"
-                ref="deadlineAt"
-          ></flat-pickr>
+          <flat-pickr v-model="Order.deadlineAt" v-validate="'date_format:YYYY-MM-DD HH:mm|required'" :config="config" class="form-control" placeholder="Select date" name="deadlineAt" ref="deadlineAt"></flat-pickr>
         </div>
         <div class="invalid-feedback-not-work">{{ errors.first('deadlineAt')}}</div>
       </div>
       <div class="form-group col-md-6">
         <label for="name">Delivery</label>
         <div>
-          <flat-pickr 
-                v-model="Order.deliveryAt"
-                v-validate="'date_format:YYYY-MM-DD HH:mm|after:deadlineAt'"
-                :config="config"                                                          
-                class="form-control" 
-                placeholder="Select date"
-                name="deliveryAt"
-          ></flat-pickr>
+          <flat-pickr v-model="Order.deliveryAt" v-validate="'date_format:YYYY-MM-DD HH:mm|after:deadlineAt'" :config="config" class="form-control" placeholder="Select date" name="deliveryAt"></flat-pickr>
           <div class="invalid-feedback-not-work">{{ errors.first('deliveryAt')}}</div>
         </div>
       </div>
@@ -74,7 +59,7 @@
           dateFormat: 'Y-m-d H:i',
           enableTime: true,
           time_24hr: true
-        },    
+        },
       };
     },
     created() {
@@ -88,8 +73,8 @@
     },
     methods: {
       create: async function() {
-        var isValid  = await this.$validator.validateAll();
-
+        var isValid = await this.$validator.validateAll();
+  
         if (isValid && !this.errors.any()) {
           OrderService.store(this.Order)
             .then(order => {
@@ -101,8 +86,8 @@
         }
       },
       edit: async function() {
-        var isValid  = await this.$validator.validateAll();
-
+        var isValid = await this.$validator.validateAll();
+  
         if (isValid && !this.errors.any()) {
           OrderService.update(this.Order)
             .then(order => {
@@ -113,12 +98,20 @@
             });
         }
       },
-     
-    },
-    components: {
-     flatPickr
-    }
-  };
+  
+      myFunction: function() {
+        // set text "Select vendor"
+        // display all products form the vendor
+        // update products after created order
+        // not possible create button unless products are chosen
+        console.log('changed!')
+      }
+  
+      },
+      components: {
+        flatPickr
+      }
+    };
 </script>
 
 <style lang="scss" scoped>
