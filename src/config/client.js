f28481f86e7f3dcc7eef4498d07ea9d980eb3caf
Marek Firlejczyk <marekfirlejczyk@gmail.com>
2019-01-30 00:04:55 +0100

    WIP

--- src/config/client.js
+++ src/config/client.js
@@ -4,9 +4,8 @@ const client = axios.create({
   baseURL: process.env.VUE_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
-    Accept: "application/json",
-    Authorization: `Bearer ${localStorage.getItem("token")}`,
-    "Access-Control-Allow-Origin": "*"
+    Accept: "application/json"
+    // Authorization: `Bearer ${localStorage.getItem("token")}`,
   }
 });
 
